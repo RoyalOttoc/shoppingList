1cce9d8a6dbd66d440aedd6e2d5e039e2a6ec89f
RoyalOttoc <53434429+RoyalOttoc@users.noreply.github.com>
2020-09-22 02:57:38 +1000

    refactoring
    data-id added

--- main.js
+++ main.js
@@ -19,22 +19,32 @@ const addItem = () => {
 function createItem(input) {
   const li = document.createElement("li");
   li.setAttribute("class", "item__row");
+  li.setAttribute("data-id", "id");
 
-  const div = document.createElement("div");
-  div.setAttribute("class", "item");
-  const span = document.createElement("span");
-  span.setAttribute("class", "item__name");
-  span.innerText = input.value;
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
-  deleteBtn.innerHTML = "❌";
+  // const div = document.createElement("div");
+  // div.setAttribute("class", "item");
+  // const span = document.createElement("span");
+  // span.setAttribute("class", "item__name");
+  // span.innerText = input.value;
+  // const deleteBtn = document.createElement("button");
+  // deleteBtn.setAttribute("class", "item__delete");
+  // deleteBtn.innerHTML = "❌";
 
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(li);
-  });
-  div.appendChild(span);
-  div.appendChild(deleteBtn);
-  li.appendChild(div);
+  // deleteBtn.addEventListener("click", () => {
+  //   items.removeChild(li);
+  // });
+  // div.appendChild(span);
+  // div.appendChild(deleteBtn);
+  // li.appendChild(div);
+  // return li;
+  let id = 0;
+  li.innerHTML = `
+  <div class="item" data-id=${id}>
+    <span class="item__name">${input.value}</span>
+    <button class="item__delete" ><i class="fas fa-trash-alt" data-id=${id}></i></button>
+  </div>
+  `;
+  id++;
   return li;
 }
 addBtn.addEventListener("click", () => {
@@ -45,5 +55,12 @@ input.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     addItem();
     items.scrollIntoView({ block: "end" });
+  }
+});
+items.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
